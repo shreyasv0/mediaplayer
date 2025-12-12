a31bab7cd8a74d23246f2f41f7bfaead7686729b
shreyas <39242611+shreyasv0@users.noreply.github.com>
2025-12-11 21:56:12 -0500

    Update sketch.js
    Added album art and accent color extraction to the visualizer.

--- sketch.js
+++ sketch.js
@@ -1,9 +1,16 @@
 let song;
 let fft;
 
+// UI elements
 let playBtn, volumeSlider, volumeIcon;
+
+// Icons
 let playImg, pauseImg;
 let vol0Img, vol25Img, vol50Img, vol100Img;
+
+// Album art + extracted color
+let albumCover;
+let accentColor;
 
 function preload() {
   song = loadSound("assets/song.mp3");
@@ -16,6 +23,9 @@ function preload() {
   vol25Img = loadImage("img/volume25.png");
   vol50Img = loadImage("img/volume50.png");
   vol100Img = loadImage("img/volume100.png");
+
+  // Album cover
+  albumCover = loadImage("img/cover.jpg");
 }
 
 function setup() {
@@ -24,13 +34,16 @@ function setup() {
 
   fft = new p5.FFT();
 
-  // HTML elements
+  // UI connections
   playBtn = select("#playBtn");
   volumeSlider = select("#volumeSlider");
   volumeIcon = select("#volumeIcon");
 
   playBtn.mousePressed(togglePlay);
   volumeSlider.input(updateVolumeIcon);
+
+  // Extract average color from album cover (accent)
+  accentColor = getAverageColor(albumCover);
 }
 
 function togglePlay() {
@@ -47,23 +60,24 @@ function updateVolumeIcon() {
   let v = volumeSlider.value();
   song.setVolume(v / 100);
 
-  if (v == 0) {
-    volumeIcon.elt.src = "img/volume0.png";
-  } else if (v <= 25) {
-    volumeIcon.elt.src = "img/volume25.png";
-  } else if (v <= 74) {
-    volumeIcon.elt.src = "img/volume50.png";
-  } else {
-    volumeIcon.elt.src = "img/volume100.png";
-  }
+  if (v == 0) volumeIcon.elt.src = "img/volume0.png";
+  else if (v <= 25) volumeIcon.elt.src = "img/volume25.png";
+  else if (v <= 74) volumeIcon.elt.src = "img/volume50.png";
+  else volumeIcon.elt.src = "img/volume100.png";
 }
 
 function draw() {
-  background(0);
+  // 1. Draw album art as the main background
+  image(albumCover, 0, 0, width, height);
 
+  // 2. Blurred accent glow from album art
+  drawAccentGlow();
+
+  // 3. Draw FFT visualizer on top
   let spectrum = fft.analyze();
+
   noStroke();
-  fill(0, 200, 255);
+  fill(255);
 
   let barWidth = width / spectrum.length * 4;
 
@@ -72,4 +86,41 @@ function draw() {
     let h = -height + map(spectrum[i], 0, 255, height, 0);
     rect(x, height, barWidth, h);
   }
+}
+
+// ----------------------------------
+// FUNCTIONS FOR COLOR + GLOW EFFECT
+// ----------------------------------
+
+function getAverageColor(img) {
+  img.loadPixels();
+
+  let r = 0, g = 0, b = 0;
+  let count = img.width * img.height;
+
+  for (let i = 0; i < img.pixels.length; i += 4) {
+    r += img.pixels[i];
+    g += img.pixels[i + 1];
+    b += img.pixels[i + 2];
+  }
+
+  return color(r / count, g / count, b / count);
+}
+
+function drawAccentGlow() {
+  push();
+  noStroke();
+
+  // Gradient-like blur circles
+  for (let i = 600; i > 0; i -= 40) {
+    let alpha = map(i, 600, 0, 20, 180);
+    fill(red(accentColor), green(accentColor), blue(accentColor), alpha);
+    ellipse(width / 2, height / 2, i);
+  }
+
+  pop();
+
+  // Darken slightly to keep visualizer readable
+  fill(0, 100);
+  rect(0, 0, width, height);
 }
