590e62003548d4f50ff0b6a2c3cc3f2f17661890
shreyas <39242611+shreyasv0@users.noreply.github.com>
2025-12-11 21:07:31 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,27 +1,60 @@
 let song;
 let fft;
-let playButton;
-let volumeSlider;
+
+let playBtn, volumeSlider, volumeIcon;
+let playImg, pauseImg;
+let vol0Img, vol25Img, vol50Img, vol100Img;
 
 function preload() {
-  song = loadSound("assets/song.mp3"); // change name if needed
+  song = loadSound("assets/song.mp3");
+
+  // Load icons
+  playImg = loadImage("img/play.png");
+  pauseImg = loadImage("img/pause.png");
+
+  vol0Img = loadImage("img/volume0.png");
+  vol25Img = loadImage("img/volume25.png");
+  vol50Img = loadImage("img/volume50.png");
+  vol100Img = loadImage("img/volume100.png");
 }
 
 function setup() {
-  createCanvas(800, 400);
+  let canvas = createCanvas(800, 400);
+  canvas.parent("playerContainer");
+
   fft = new p5.FFT();
 
-  playButton = createButton("Play / Pause");
-  playButton.mousePressed(toggleSong);
+  // HTML elements
+  playBtn = select("#playBtn");
+  volumeSlider = select("#volumeSlider");
+  volumeIcon = select("#volumeIcon");
 
-  volumeSlider = createSlider(0, 1, 0.5, 0.01);
+  playBtn.mousePressed(togglePlay);
+  volumeSlider.input(updateVolumeIcon);
 }
 
-function toggleSong() {
+function togglePlay() {
   if (song.isPlaying()) {
     song.pause();
+    playBtn.elt.src = "img/play.png";
   } else {
     song.play();
+    playBtn.elt.src = "img/pause.png";
+  }
+}
+
+function updateVolumeIcon() {
+  let v = volumeSlider.value();
+  song.setVolume(v / 100);
+
+  if (v == 0) {
+    volumeIcon.elt.src = "img/volume0.png";
+  } else if (v <= 25) {
+    volumeIcon.elt.src = "img/volume25.png";
+  } else if (v <= 74) {
+    volumeIcon.elt.src = "img/volume50.png";
+  } else {
+    volumeIcon.elt.src = "img/volume100.png";
   }
 }
 
@@ -29,15 +62,14 @@ function draw() {
   background(0);
 
   let spectrum = fft.analyze();
-
   noStroke();
-  fill(0, 255, 200);
+  fill(0, 200, 255);
 
-  for (let i = 0; i < spectrum.length; i += 10) {
+  let barWidth = width / spectrum.length * 4;
+
+  for (let i = 0; i < spectrum.length; i += 4) {
     let x = map(i, 0, spectrum.length, 0, width);
     let h = -height + map(spectrum[i], 0, 255, height, 0);
-    rect(x, height, width / spectrum.length * 10, h);
+    rect(x, height, barWidth, h);
   }
-
-  song.setVolume(volumeSlider.value());
 }
